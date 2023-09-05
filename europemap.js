4d41e5836a95b4b90e8d9a4e3a745232ff7b9c81
Edmond_Liu <jl2058@hw.ac.uk>
2023-09-06 03:14:57 +0800

    add click function

--- europemap.js
+++ europemap.js
@@ -1,90 +1,40 @@
-// import { mapData } from "./loadData.js";
+const mapData = await d3.json("./data/europe.geojson");
 
-// const width = document.getElementById('europemap').getBoundingClientRect().width;
-// const height = document.getElementById('europemap').getBoundingClientRect().height;
+const width = document.getElementById('europemap').getBoundingClientRect().width;
+const height = document.getElementById('europemap').getBoundingClientRect().height;
 
-// const projection = d3.geoMercator() //  d3 geographic projection
-//     .fitSize([width, height], mapData);
+const projection = d3.geoMercator()
+    .fitSize([width, height], mapData);
 
-// const pathGenerator = d3.geoPath().projection(projection);
+const pathGenerator = d3.geoPath().projection(projection);
 
-// const svg = d3.select("#europemap");
+const svg = d3.select("#europemap");
 
-// // Tooltip to show country name
-// const tooltip = d3.select("body").append("div")
-//     .attr("class", "tooltip")
-//     .style("position", "absolute")
-//     .style("background-color", "white")
-//     .style("border", "solid")
-//     .style("border-width", "10px")
-//     .style("border-radius", "15px")
-//     .style("padding", "10px")
-//     .style("opacity", 0);
+// Tooltip for country name on hover
+const tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("position", "absolute")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "1px")
+    .style("border-radius", "5px")
+    .style("padding", "10px")
+    .style("opacity", 0);
 
-// async function drawMap() {
+// Details div for showing country details on click
+const details = d3.select("body").append("div")
+    .attr("class", "details")
+    .style("position", "fixed")
+    .style("top", "10px")
+    .style("left", "10px")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "1px")
+    .style("border-radius", "5px")
+    .style("padding", "10px")
+    .style("opacity", 0);
 
-//     svg.selectAll("path")
-//         .data(mapData.features)
-//         .enter()
-//         .append("path")
-//         .attr("d", pathGenerator)
-//         .attr("fill", "#ccc")
-//         .attr("stroke", "#000")
-//         .on("mouseover", function (event, d) {  // Mouseover effect
-//             tooltip.transition()
-//                 .duration(200)
-//                 .style("opacity", 1);
-//             tooltip.html(d.properties.NAME)   // Assuming 'name' is the property in your geojson file
-//                 .style("left", (event.pageX + 10) + "px")
-//                 .style("top", (event.pageY - 28) + "px");
-//             // Dim other countries
-//             countries.transition().duration(200)
-//                 .style("opacity", c => (c === d ? 1 : 0.2));
-//         })
-//         .on("mouseout", function (d) {  // Mouseout effect
-//             tooltip.transition()
-//                 .duration(500)
-//                 .style("opacity", 0);
-//         });
-// }
-
-// drawMap();
 async function drawMap() {
-    const mapData = await d3.json("./data/europe.geojson");
-
-    const width = document.getElementById('europemap').getBoundingClientRect().width;
-    const height = document.getElementById('europemap').getBoundingClientRect().height;
-
-    const projection = d3.geoMercator()
-        .fitSize([width, height], mapData);
-
-    const pathGenerator = d3.geoPath().projection(projection);
-
-    const svg = d3.select("#europemap");
-
-    // Tooltip for country name on hover
-    const tooltip = d3.select("body").append("div")
-        .attr("class", "tooltip")
-        .style("position", "absolute")
-        .style("background-color", "white")
-        .style("border", "solid")
-        .style("border-width", "1px")
-        .style("border-radius", "5px")
-        .style("padding", "10px")
-        .style("opacity", 0);
-
-    // Details div for showing country details on click
-    const details = d3.select("body").append("div")
-        .attr("class", "details")
-        .style("position", "fixed")
-        .style("top", "10px")
-        .style("left", "10px")
-        .style("background-color", "white")
-        .style("border", "solid")
-        .style("border-width", "1px")
-        .style("border-radius", "5px")
-        .style("padding", "10px")
-        .style("opacity", 0);
 
     const countries = svg.selectAll("path")
         .data(mapData.features)
